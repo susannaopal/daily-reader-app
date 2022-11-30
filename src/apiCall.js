a49666ec6d03092d1f5fb261fb1411565dcde58b
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-11-30 08:30:47 -0500

    Update to remove public key

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,5 +1,5 @@
 export const fetchHeadlines = () => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=4bxWhQUEJsgqskrAo23GYzh6xYoBnPOE`)
+  return fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=4bxWhQUEJsgqskrAo23GYzh6xYoBnPOE')
     .then(res => {
       if (res.ok) {
         return res.json()
