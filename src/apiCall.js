1b901c94b74129c53279dd4f345a931391844d2b
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-11-30 08:56:39 -0500

    Troubleshooting app call issue with key

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,13 +1,13 @@
-const API_KEY = process.env.REACT_APP_API_KEY 
+const apiKey = process.env.REACT_APP_API_KEY 
 
 export const fetchHeadlines = () => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`)
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`)
     .then(res => {
       if (res.ok) {
         return res.json()
       } else {
         throw new Error()
-      }
+      } 
     })
     .catch(err => err)
-}
+};
