f8c05ef14f32cc5364ce1968f9141ab7850e9440
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2022-11-30 12:01:10 -0500

    Update file structure to run app with key correctly added

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,6 +1,9 @@
 const apiKey = process.env.REACT_APP_API_KEY 
+console.log("1-apiKey", apiKey)
 
 export const fetchHeadlines = () => {
+    console.log("2-apiKey", apiKey)
+
   return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`)
     .then(res => {
       if (res.ok) {
